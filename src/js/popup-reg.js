52f4f169a4c9a8531ee86f9171904344b12b507a
kulakovskyi <torchok9@gmail.com>
2023-05-05 18:00:01 +0300

    add new fix

--- src/js/popup-reg.js
+++ src/js/popup-reg.js
@@ -1,18 +1,3 @@
-const popupBtnOpen = document.querySelector('.popup-btn-click')
-const popupBtnClose = document.querySelector('.popup-reg__info-btn-close')
-const popUpSubscribe = document.querySelector('.popup-reg')
-const body = document.querySelector('body')
-
-popupBtnOpen.addEventListener('click', () => {
-    body.classList.add('_lock')
-    popUpSubscribe.classList.remove('_hidden')
-})
-
-popupBtnClose.addEventListener('click', () => {
-    body.classList.remove('_lock')
-    popUpSubscribe.classList.add('_hidden')
-})
-
 //input password
 
 const iconPasswordShow = document.querySelector('.icon-pass')
--- src/js/short3.js
+++ src/js/short3.js
@@ -1,17 +1,3 @@
-const popupBtnOpen = document.querySelector('.popup-btn-click')
-const popupBtnClose = document.querySelector('.popup-login__info-btn-close')
-const popUpSubscribe = document.querySelector('.popup-login')
-const body = document.querySelector('body')
-
-popupBtnOpen.addEventListener('click', () => {
-    body.classList.add('_lock')
-    popUpSubscribe.classList.remove('_hidden')
-})
-
-popupBtnClose.addEventListener('click', () => {
-    body.classList.remove('_lock')
-    popUpSubscribe.classList.add('_hidden')
-})
 
 //input password
 
